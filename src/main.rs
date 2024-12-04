e03fe327df124ed96e41fefa92eceeea8379db4e
kouhe3 <25522053+kouhe3@users.noreply.github.com>
2024-12-04 17:52:08 +0800

    重构

--- src/main.rs
+++ src/main.rs
@@ -29,129 +29,123 @@ struct Food {
     y: u32,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Direction {
-    Up,
-    Down,
     Left,
+    Up,
     Right,
+    Down,
 }
 
-static FOOD: Mutex<Food> = Mutex::new(Food { x: 0, y: 0 });
-static STAGE: Stage = Stage { x: 100, y: 100 };
-static DIRECTION: LazyLock<Arc<Mutex<Direction>>> =
-    LazyLock::new(|| Arc::new(Mutex::new(Direction::Up)));
-static SNAKE: Mutex<VecDeque<Body>> = Mutex::new(VecDeque::new());
-
-fn add_snake_head() {
-    let mut snake = SNAKE.lock().unwrap();
-    let direct = DIRECTION.lock().unwrap();
-    let new_head = match *direct {
-        Direction::Up => Body {
-            x: snake[0].x,
-            y: snake[0].y - 1,
-        },
-        Direction::Down => Body {
-            x: snake[0].x,
-            y: snake[0].y + 1,
-        },
-        Direction::Left => Body {
-            x: snake[0].x - 1,
-            y: snake[0].y,
-        },
-        Direction::Right => Body {
-            x: snake[0].x + 1,
-            y: snake[0].y,
-        },
-    };
-    snake.push_front(new_head);
-}
-
-fn del_snake_tail() {
-    let mut snake = SNAKE.lock().unwrap();
-    let food = FOOD.lock().unwrap();
-    let eaten = snake[0].x == food.x && snake[0].y == food.y;
-    if !eaten {
-        //del_snake_tail
-        snake.pop_back();
-    } else {
-        new_food();
+impl Direction {
+    fn reverse(&self) -> Self {
+        match self {
+            Direction::Left => Direction::Right,
+            Direction::Up => Direction::Down,
+            Direction::Right => Direction::Left,
+            Direction::Down => Direction::Up,
+        }
     }
 }
 
-#[allow(non_snake_case)]
-unsafe extern "system" fn LowLevelKeyboardProc(
-    nCode: i32,
-    wParam: WPARAM,
-    lParam: LPARAM,
-) -> LRESULT {
-    if nCode as u32 == HC_ACTION {
-        let kb_data = unsafe { *(lParam.0 as *mut KBDLLHOOKSTRUCT) };
-        if wParam.0 as u32 == WM_KEYDOWN {
-            match kb_data.vkCode {
-                0x25 => *DIRECTION.lock().unwrap() = Direction::Left,
-                0x26 => *DIRECTION.lock().unwrap() = Direction::Up,
-                0x27 => *DIRECTION.lock().unwrap() = Direction::Right,
-                0x28 => *DIRECTION.lock().unwrap() = Direction::Down,
-                _ => {}
-            }
+struct Snake {
+    body: Mutex<VecDeque<Body>>,
+    direction: Mutex<Direction>,
+}
+
+struct Game {
+    snake: Mutex<Snake>,
+    player_input: Arc<Mutex<Direction>>,
+    food: Food,
+    score: u32,
+    stage: Stage,
+    game_over: bool,
+}
+
+impl Game {
+    fn new(x: u32, y: u32) -> Self {
+        let mut rng = rand::thread_rng();
+        let snake = Snake::new(Stage { x: x, y: y });
+        let food = Food {
+            x: rng.gen_range(0..x),
+            y: rng.gen_range(0..y),
+        };
+        let player_input = Arc::new(Mutex::new(Direction::Right));
+        Game {
+            snake: Mutex::new(snake),
+            player_input,
+            food,
+            score: 0,
+            stage: Stage { x: x, y: y },
+            game_over: false,
         }
     }
 
-    unsafe { CallNextHookEx(None, nCode, wParam, lParam) }
+    fn step(&mut self){
+        let mut snake = self.snake.lock().unwrap();
+        snake.add_head(Direction::Right);
+        snake.del_tail();
+        
+    }
 }
 
-fn new_food() {
-    let mut rng = rand::thread_rng();
-    let mut food = FOOD.lock().unwrap();
-    food.x = rng.gen_range(0..STAGE.x);
-    food.y = rng.gen_range(0..STAGE.y);
-}
+impl Snake {
+    fn new(stage: Stage) -> Self {
+        let mut body = VecDeque::new();
+        let mut rng = rand::thread_rng();
+        let start_x = rng.gen_range(0..stage.x);
+        let start_y = rng.gen_range(0..stage.y);
+        for _ in 0..2 {
+            body.push_back(Body {
+                x: start_x,
+                y: start_y,
+            });
+        }
+        Snake {
+            body: Mutex::new(body),
+            direction: Mutex::new(Direction::Right),
+        }
+    }
 
-fn init_snake() {
-    let mut rng = rand::thread_rng();
-    let mut snake = SNAKE.lock().unwrap();
-    let start_x: u32 = rng.gen_range(0..STAGE.x);
-    let start_y: u32 = rng.gen_range(0..STAGE.y);
-    snake.push_back(Body {
-        x: start_x,
-        y: start_y,
-    });
-    snake.push_back(Body {
-        x: start_x,
-        y: start_y,
-    });
+    fn add_head(&mut self, intput_direction: Direction) {
+        let mut snake = self.body.lock().unwrap();
+        let mut snake_direction = self.direction.lock().unwrap();
+        //check if player input oppside snake self,
+        //if oppside then just ignore player input do not change current direction
+        if snake_direction.reverse() != intput_direction {
+            *snake_direction = intput_direction;
+        }
+        let new_head = match *snake_direction {
+            Direction::Up => Body {
+                x: snake[0].x,
+                y: snake[0].y - 1,
+            },
+            Direction::Down => Body {
+                x: snake[0].x,
+                y: snake[0].y + 1,
+            },
+            Direction::Left => Body {
+                x: snake[0].x - 1,
+                y: snake[0].y,
+            },
+            Direction::Right => Body {
+                x: snake[0].x + 1,
+                y: snake[0].y,
+            },
+        };
+        snake.push_front(new_head);
+        drop(snake_direction);
+        drop(snake);
+    }
+
+    fn del_tail(&mut self) {
+        let mut snake = self.body.lock().unwrap();
+        snake.pop_back();
+        drop(snake);
+    }
 }
 
 fn main() {
-    thread::spawn(create_hook);
-    init_snake();
-    loop {
-        show_snake_info();
-        println!("Waiting input");
-        thread::sleep(Duration::from_secs(1));
-        add_snake_head();
-        del_snake_tail();
-    }
-}
+    let game = Game::new(100, 100);
 
-fn create_hook() {
-    //start hook for listen keyboard input
-    let hh = unsafe { SetWindowsHookExA(WH_KEYBOARD_LL, Some(LowLevelKeyboardProc), None, 0) };
-    if hh.is_err() {
-        panic!("hook error");
-    }
-    //msg loop
-    unsafe {
-        let mut msg = std::mem::zeroed();
-        while GetMessageA(&mut msg, None, 0, 0).0 != 0 {
-            TranslateMessage(&mut msg);
-            DispatchMessageA(&mut msg);
-        }
-    }
-}
-
-fn show_snake_info() {
-    println!("{:?}", *DIRECTION.lock().unwrap());
-    println!("{:?}", *SNAKE.lock().unwrap());
 }
